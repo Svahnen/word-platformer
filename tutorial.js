8ef0e793638a6fb9bd3996a2125e2e9161274b1e
unknown <svahn.jonny@gmail.com>
2019-04-26 15:41:12 +0200

    Add tutorial

--- tutorial.js
+++ tutorial.js
@@ -1,10 +1,10 @@
 let tutorial = {
   preload: preload,
-  create: create,
-  update: update
+  create: create
 }
 
 function preload () {
+  this.scene.add('world', world)
   // Background assets
   this.load.image('background', 'assets/background/bg_layer2.png')
 }
@@ -13,8 +13,15 @@ function create () {
   // Set background color and background
   this.cameras.main.setBackgroundColor('#ffffff')
   this.add.image(500, 350, 'background').setScale(0.55)
-}
-
-function update () {
-
+  tutorialWalkRight()
+  setTimeout(() => {
+    tutorialWalkLeft()
+  }, 3500)
+  setTimeout(() => {
+    tutorialJump()
+  }, 7000)
+  setTimeout(() => {
+    document.querySelector('#tutorial').classList = 'displayNone'
+    this.scene.start('world', world)
+  }, 10000)
 }
